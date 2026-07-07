1261c54fc2720619af7c1b40bf0464e612aadfa9
Geiliane777 <geiliane.tomaz@odontogroup.com.br>
2026-07-07 14:56:57 -0300

    Update especialistas.js

--- js/especialistas.js
+++ js/especialistas.js
@@ -1,13 +1,1 @@
-// ================================
-// REDE ESPECIALISTAS
-// ================================
-
-document
-    .getElementById("buscar")
-    .addEventListener("click", buscarClinicas);
-
-async function buscarClinicas(){
-
-    alert("Botão funcionando!");
-
-}
+console.log("especialistas.js carregado");
