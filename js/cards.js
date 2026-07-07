11415da2f586d5fa24cfc9140f3a097df006040f
Geiliane777 <geiliane.tomaz@odontogroup.com.br>
2026-07-07 15:20:52 -0300

    Update cards.js

--- js/cards.js
+++ js/cards.js
@@ -1,39 +1,24 @@
-// ================================
-// EXIBIR CLÍNICAS NA TELA
-// ================================
-
 function mostrarClinicas(clinicas) {
+
+    console.log("mostrarClinicas foi chamada");
 
     const resultado = document.getElementById("resultado");
 
+    console.log(resultado);
+
     resultado.innerHTML = "";
-
-    if (clinicas.length === 0) {
-
-        resultado.innerHTML = `
-            <p>Nenhuma clínica encontrada.</p>
-        `;
-
-        return;
-    }
 
     clinicas.forEach(clinica => {
 
         resultado.innerHTML += `
-            <div class="card">
-
+            <div style="
+                border:1px solid #000;
+                margin:15px;
+                padding:15px;
+                background:white;
+            ">
                 <h2>${clinica.nome}</h2>
-
-                <p>
-                    <strong>📍 Endereço:</strong><br>
-                    ${clinica.endereco}
-                </p>
-
-                <p>
-                    <strong>📞 Telefone:</strong><br>
-                    ${clinica.telefone ?? "Não informado"}
-                </p>
-
+                <p>${clinica.endereco}</p>
             </div>
         `;
 
