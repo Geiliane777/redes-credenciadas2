4fd2295891693f09b724fc06262e8eddbe6b2d6e
Geiliane777 <geiliane.tomaz@odontogroup.com.br>
2026-07-07 14:59:35 -0300

    Update especialistas.js

--- js/especialistas.js
+++ js/especialistas.js
@@ -1,1 +1,9 @@
 console.log("especialistas.js carregado");
+
+const botao = document.getElementById("buscar");
+
+console.log(botao);
+
+botao.addEventListener("click", () => {
+    alert("Botão funcionando!");
+});
