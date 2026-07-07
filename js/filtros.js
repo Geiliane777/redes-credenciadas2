dac1370627d54feeb686bed816b06527a5927f5f
Geiliane777 <geiliane.tomaz@odontogroup.com.br>
2026-07-07 14:40:05 -0300

    Update filtros.js

--- js/filtros.js
+++ js/filtros.js
@@ -4,10 +4,15 @@
 
 async function carregarRegioes() {
 
+    console.log("Buscando regiões...");
+
     const { data, error } = await supabaseClient
         .from("regioes")
         .select("*")
         .order("nome");
+
+    console.log(data);
+    console.log(error);
 
     if (error) {
         console.error(error);
@@ -16,18 +21,14 @@ async function carregarRegioes() {
 
     const select = document.getElementById("regiao");
 
-    select.innerHTML = `
-        <option value="">Selecione a Região</option>
-    `;
+    select.innerHTML = `<option value="">Selecione a Região</option>`;
 
     data.forEach(regiao => {
-
         select.innerHTML += `
             <option value="${regiao.id}">
                 ${regiao.nome}
             </option>
         `;
-
     });
 
 }
