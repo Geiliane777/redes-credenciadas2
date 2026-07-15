a461b95d960db9b3a438088ccf32fd7ef82b8a70
Geiliane777 <geiliane.tomaz@odontogroup.com.br>
2026-07-15 12:54:09 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,11 +1,8 @@
 // =============================
 // CONFIGURAÇÃO DO SUPABASE
 // =============================
-
 const SUPABASE_URL = "https://mmmiqqxienjeioxcnggu.supabase.co";
-
 const SUPABASE_KEY = "sb_publishable_ZHE8lc4B6IgyqDLe_LRK7w_edpWgmSz";
-
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
