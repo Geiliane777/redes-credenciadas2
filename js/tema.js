7b49ce5d083e457353369f29cf203e1b1c5a92d4
Geiliane777 <geiliane.tomaz@odontogroup.com.br>
2026-07-15 12:54:32 -0300

    Update tema.js

--- js/tema.js
+++ js/tema.js
@@ -3,66 +3,35 @@
 // ======================================
 console.log("tema.js carregado");
 document.addEventListener("DOMContentLoaded", () => {
-
     const btnTema = document.getElementById("btnTema");
-
     if (!btnTema) return;
-
-
     // ======================================
     // CARREGAR TEMA SALVO
     // ======================================
-
     const temaSalvo = localStorage.getItem("tema");
-
     if (temaSalvo === "dark") {
-
         document.body.classList.add("dark");
-
         btnTema.innerHTML = "☀️";
-
         btnTema.title = "Tema Claro";
-
     } else {
-
         document.body.classList.remove("dark");
-
         btnTema.innerHTML = "🌙";
-
         btnTema.title = "Tema Escuro";
-
     }
-
-
     // ======================================
     // ALTERAR TEMA
     // ======================================
-
     btnTema.addEventListener("click", () => {
-
         document.body.classList.toggle("dark");
-
         const dark = document.body.classList.contains("dark");
-
-
         if (dark) {
-
             localStorage.setItem("tema", "dark");
-
             btnTema.innerHTML = "☀️";
-
             btnTema.title = "Tema Claro";
-
         } else {
-
             localStorage.setItem("tema", "light");
-
             btnTema.innerHTML = "🌙";
-
             btnTema.title = "Tema Escuro";
-
         }
-
     });
-
 });
